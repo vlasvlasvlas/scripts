8502aac84f31361f50f96c5cd8b36f18e4dd4af6
Vladimiro Bellini <vbellini@mail.austral.edu.ar>
2021-05-24 12:51:40 -0300

    Update catalogo_unicadehistorico.py

--- python/ocds/dncp_datosabiertos/catalogo_unicadehistorico.py
+++ python/ocds/dncp_datosabiertos/catalogo_unicadehistorico.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
+# solo catalogo
 df = pd.read_csv('outs/ten_items.csv')
-df_clas = df['compiledRelease/tender/items/0/classification/id']
+df_clas = df['compiledRelease/tender/items/0/classification/id','aniocsv']
 df_clas_uniq = df_clas.unique()
 df_clas_uniq_s = pd.Series(df_clas_uniq)
 df_clas_uniq_s.to_csv('outs/catalogo_unicadehistorico.csv')
+
+# catalogo con anio
+dfgroup = df.groupby(['compiledRelease/tender/items/0/classification/id','aniocsv']).size().reset_index(name='Freq')
+dfunique_catalogo_anio = dfgroup[dfgroup['Freq'] == 1]
+dfunique_catalogo_anio.to_csv('outs/catalogo_unicadehistoricoanio.csv')
