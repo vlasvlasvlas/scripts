4a870e2f47077ecf603cece5866e02fc35f33838
Vladimiro Bellini <vbellini@mail.austral.edu.ar>
2021-05-26 10:27:28 -0300

    Update exploratorio2_SalidaDataProfile.py

--- python/ocds/dncp_datosabiertos/exploratorio2_SalidaDataProfile.py
+++ python/ocds/dncp_datosabiertos/exploratorio2_SalidaDataProfile.py
@@ -1,6 +1,7 @@
 import pandas as pd, sys 
 from pandas_profiling import ProfileReport
 
+# use under 'archivos' the appended csv files you like to make a pandas data profile html
 #archivos = ('records','ten_items','ten_lots','ten_notifiedSuppliers','contracts')
 archivos = ('records',)
 
@@ -8,5 +9,5 @@ for archivo in archivos:
     print(archivo)
     df = pd.read_csv(f'outs/{archivo}.csv',low_memory=False,index_col=False)
     print(df)
-    profile = ProfileReport(df, title=f"{archivo} Profiling Report", minimal=True)
-    profile.to_file(f'htmlprofile/'+archivo+'.html')
+    profile = ProfileReport(df, title=f"{archivo} Profiling Report", minimal=True) # minimal is recommended
+    profile.to_file(f'htmlprofile/'+archivo+'.html') # into folder /htmlprofile
