903458611178aff5381c9a98048178f163fc6dff
Vladimiro Bellini <vbellini@mail.austral.edu.ar>
2021-08-26 23:21:19 -0300

    Add files via upload

--- python/pandas-protected.py
+++ python/pandas-protected.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from pandas_profiling import ProfileReport
 import json
-import msoffcrypto # acordate de instalar la liberia: pip install msoffcrypto-tool # https://github.com/nolze/msoffcrypto-tool
+import msoffcrypto # pip install msoffcrypto-tool # https://github.com/nolze/msoffcrypto-tool
 import io
 
 decrypted = io.BytesIO()
@@ -12,7 +12,7 @@ filein = 'ej1.xls'
 
 with open(pathin+filein, "rb") as f:
     file = msoffcrypto.OfficeFile(f)
-    file.load_key(password="VelvetSweatshop")  # Use password
+    file.load_key(password="VelvetSweatshop")  # Use ms default password
     file.decrypt(decrypted)
 
 sheetname = 3 #INPUT
